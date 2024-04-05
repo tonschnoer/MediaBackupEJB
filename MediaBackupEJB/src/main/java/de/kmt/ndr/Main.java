1228e69cde7c0863c4e591d89d6d7fe973b7dce0
tonschnoer <kmtegtmeier@tonschnoer.de>
2024-04-05 20:30:20 +0200

    add comments, reformat source

--- MediaBackupEJB/src/main/java/de/kmt/ndr/Main.java
+++ MediaBackupEJB/src/main/java/de/kmt/ndr/Main.java
@@ -1,6 +1,5 @@
 package de.kmt.ndr;
 
-
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import jakarta.annotation.Resource;
@@ -12,58 +11,62 @@ import jakarta.ejb.Timer;
 import jakarta.ejb.TimerConfig;
 import jakarta.ejb.TimerService;
 
-
 @Singleton
 @Startup
 
 public class Main {
-	
-    @Resource
-    private TimerService timerService;
-    
-    private void _setUP() {
-  	
-    }
-    
-    @Timeout
-    public void scheduler(Timer timer) {
-    	try {
-    		_setUP();
-    		System.out.println("Timer fired.");
-    	}
-    	catch(Exception _ex) {
-    		
-    	}
-    }
 
-    @PostConstruct
-    public void initialize() {
-    	try {
-    		_setUP();
-            ScheduleExpression se = new ScheduleExpression();
-            se.hour("*").minute("0/1").second("0/1");
-            
-            timerService.createCalendarTimer(se, new TimerConfig("Cleanup Service scheduled at ", false));
-    	}
-    	catch(Exception _ex) {
-    		
-    	}
-    }
+	@Resource
+	private TimerService timerService;
 
-    @PreDestroy
-    public void stop() {
-    	
-    	try {
-    		_setUP();
-	        for (Timer timer : timerService.getTimers()) {
-        	
-	            timer.cancel();
-	        }
-    	}
-    	catch(Exception _ex) {
-    		
-    	}
-    }
+	private void _setUP() {
+
+	}
+
+	// ***********************************************************************************
+	// triggers every time when timer fires
+	// ***********************************************************************************
+	@Timeout
+	public void scheduler(Timer timer) {
+		try {
+			_setUP();
+			System.out.println("Timer fired.");
+		} catch (Exception _ex) {
+
+		}
+	}
+
+	// ***********************************************************************************
+	// OnStart; configures timer
+	// ***********************************************************************************
+	@PostConstruct
+	public void initialize() {
+		try {
+			_setUP();
+			ScheduleExpression se = new ScheduleExpression();
+			se.hour("*").minute("0/1").second("0/1");
+
+			timerService.createCalendarTimer(se, new TimerConfig("Cleanup Service scheduled at ", false));
+		} catch (Exception _ex) {
+
+		}
+	}
+
+	// ***********************************************************************************
+	// OnStop; destroys timer
+	// ***********************************************************************************
+	@PreDestroy
+	public void stop() {
+
+		try {
+			_setUP();
+			for (Timer timer : timerService.getTimers()) {
+
+				timer.cancel();
+			}
+		} catch (Exception _ex) {
+
+		}
+	}
 
 }
-
